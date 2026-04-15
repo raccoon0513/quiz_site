8e499f5c7c51df1f63bc3c90eac4ea5a95fe9213
raccoon0513 <axm9993@gmail.com>
2026-04-15 22:36:32 +0900

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // api.js
 export async function fetchQuestions() {
   try {
-    const response = await fetch('data.json');
+    const response = await fetch('../quiz/data.json');
     return await response.json();
   } catch (error) {
     console.error("데이터를 불러오는 데 실패했습니다:", error);
